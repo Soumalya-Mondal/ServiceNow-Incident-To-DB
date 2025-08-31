760594bc9846bb7a0abc5d3a1dbc6e9b65260e5e
Soumalya-Mondal <iamsoumalya007@hotmail.com>
2025-08-31 20:24:16 +0530

    Adding Log Writer Features

--- support/dbtabledetails.py
+++ support/dbtabledetails.py
@@ -2,11 +2,20 @@
 def db_table_details(db_name: str, username: str, password: str, db_host: str, db_port: str) -> dict[str, str]:
     # importing python module:S01
     try:
+        from pathlib import Path
+        import sys
         import psycopg2
     except Exception as error:
         return {'status' : 'ERROR', 'message' : f'[DB-Table-Details:S01] - {str(error)}'}
 
-    # define db connection parameter:S02
+    # importing "log_writer" function:S02
+    try:
+        sys.path.append(str(Path.cwd()))
+        from support.logwriter import log_writer
+    except Exception as error:
+        return {'status' : 'ERROR', 'message' : f'[DB-Table-Details:S02] - {str(error)}', 'ticket_count' : '0'}
+
+    # define db connection parameter:S03
     try:
         db_connection_parameter = {
             "dbname" : str(db_name),
@@ -15,10 +24,12 @@ def db_table_details(db_name: str, username: str, password: str, db_host: str, d
             "host" : str(db_host),
             "port" : str(db_port)
         }
+        log_writer(file_name = 'DB-Table-Details', steps = '03', status = 'SUCCESS', message = 'Database Connection Parameter Defined')
     except Exception as error:
-        return {'status' : 'ERROR', 'message' : f'[DB-Table-Details:S02] - {str(error)}'}
+        log_writer(file_name = 'DB-Table-Details', steps = '03', status = 'ERROR', message = str(error))
+        return {'status' : 'ERROR', 'message' : f'[DB-Table-Details:S03] - {str(error)}'}
 
-    # check if "incident_data" table present and fetch row count:S03
+    # check if "incident_data" table present and fetch row count:S04
     try:
         with psycopg2.connect(**db_connection_parameter) as database_connection:  # type: ignore
             with database_connection.cursor() as database_cursor:
@@ -31,19 +42,20 @@ def db_table_details(db_name: str, username: str, password: str, db_host: str, d
                 table_exists = database_cursor.fetchone()[0]
                 # check if "incident_data" table present
                 if table_exists:
+                    log_writer(file_name = 'DB-Table-Details', steps = '04', status = 'SUCCESS', message = 'Table "incident_data" Already Present')
                     # fetch row count from "incident_data" table
                     database_cursor.execute('SELECT COUNT(*) FROM incident_data;')
                     records_count = database_cursor.fetchone()[0]
                     # check "records_count"
                     if (int(records_count) == 0):
+                        log_writer(file_name = 'DB-Table-Details', steps = '04', status = 'SUCCESS', message = 'Table "incident_data" Has No Data, Droping Table')
                         # drop table
                         database_cursor.execute('DROP TABLE incident_data')
-                    else:
-                        return {'status' : 'INFO', 'message' : f'"incident_data" Table Already Present; {records_count} Rows Present'}
     except Exception as error:
-        return {'status' : 'ERROR', 'message' : f'[DB-Table-Details:S03] - {str(error)}'}
+        log_writer(file_name = 'DB-Table-Details', steps = '04', status = 'ERROR', message = str(error))
+        return {'status' : 'ERROR', 'message' : f'[DB-Table-Details:S04] - {str(error)}'}
 
-    # define "incident_data" table create SQL:S04
+    # define "incident_data" table create SQL:S05
     try:
         incident_data_table_create_sql = '''
         CREATE TABLE incident_data (
@@ -83,18 +95,21 @@ def db_table_details(db_name: str, username: str, password: str, db_host: str, d
             CONSTRAINT number_not_blank CHECK (btrim(number) <> '')
         );
         ALTER TABLE incident_data OWNER TO soumalya;'''
+        log_writer(file_name = 'DB-Table-Details', steps = '05', status = 'SUCCESS', message = 'Table Create SQL Define For "incident_data"')
     except Exception as error:
-        return {'status' : 'ERROR', 'message' : f'[DB-Table-Details:S04] - {str(error)}'}
+        log_writer(file_name = 'DB-Table-Details', steps = '05', status = 'ERROR', message = str(error))
+        return {'status' : 'ERROR', 'message' : f'[DB-Table-Details:S05] - {str(error)}'}
 
-    # create "incident_data" table:S05
+    # create "incident_data" table:S06
     try:
         with psycopg2.connect(**db_connection_parameter) as database_connection: # type: ignore
             with database_connection.cursor() as database_cursor:
                 database_cursor.execute(incident_data_table_create_sql)
     except Exception as error:
-        return {'status' : 'ERROR', 'message' : f'[DB-Table-Details:S05] - {str(error)}'}
+        log_writer(file_name = 'DB-Table-Details', steps = '06', status = 'ERROR', message = str(error))
+        return {'status' : 'ERROR', 'message' : f'[DB-Table-Details:S06] - {str(error)}'}
 
-    # check if "incident_data" table create:S06
+    # check if "incident_data" table create:S07
     try:
         with psycopg2.connect(**db_connection_parameter) as database_connection:  # type: ignore
             with database_connection.cursor() as database_cursor:
@@ -107,8 +122,11 @@ def db_table_details(db_name: str, username: str, password: str, db_host: str, d
                 table_exists = database_cursor.fetchone()[0]
                 # check if "incident_data" table present
                 if table_exists:
+                    log_writer(file_name = 'DB-Table-Details', steps = '07', status = 'SUCCESS', message = 'Table "incident_data" Created')
                     return {'status' : 'SUCCESS', 'message' : '"incident_data" Table Created'}
                 else:
+                    log_writer(file_name = 'DB-Table-Details', steps = '07', status = 'ERROR', message = 'Table "incident_data" Not Created')
                     return {'status' : 'ERROR', 'message' : '"incident_data" Table Not Created'}
     except Exception as error:
-        return {'status' : 'ERROR', 'message' : f'[DB-Table-Details:S06] - {str(error)}'}
+        log_writer(file_name = 'DB-Table-Details', steps = '07', status = 'ERROR', message = str(error))
+        return {'status' : 'ERROR', 'message' : f'[DB-Table-Details:S07] - {str(error)}'}
