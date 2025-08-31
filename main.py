509130e9836f9dfc8a7484a9b808224b5a15b1dd
Soumalya-Mondal <iamsoumalya007@hotmail.com>
2025-08-31 12:32:11 +0530

    Adding Total Ticket Count Functionality In Main Script

--- main.py
+++ main.py
@@ -14,31 +14,34 @@ if __name__ == '__main__':
 
     # define folder and file path:S02
     try:
+        sys.path.append(str(Path.cwd()))
         parent_folder_path = Path.cwd()
         env_file_path = Path(parent_folder_path) / '.env'
     except Exception as error:
         print(f'ERROR - [S02] - {str(error)}')
 
-    # check if ".env" file is present and valid:S03
+    # importing user-define module:S03
     try:
-        if (not (env_file_path.exists() and env_file_path.is_file())):
-            print(f'ERROR - ".env" File Is Not Present')
-            sys.exit(1)
+        from support.totalticketcount import total_ticket_count
     except Exception as error:
         print(f'ERROR - [S03] - {str(error)}')
 
-    # load ".env" values:S04
+    # check if ".env" file is present and valid:S04
     try:
-        environment_values = dotenv_values(env_file_path)
-        snow_api_url_for_stats = str(environment_values.get('SNOW_URL_STATS', 'NONE'))
-        snow_api_url_for_data = str(environment_values.get('SNOW_URL_DATA', 'NONE'))
-        snow_username = str(environment_values.get('SNOW_USERNAME', 'NONE'))
-        snow_password = str(environment_values.get('SNOW_PASSWORD', 'NONE'))
-        pg_host = str(environment_values.get('PG_HOST', 'LOCALHOST'))
-        pg_port = str(environment_values.get('PG_PORT', '5432'))
-        pg_database = str(environment_values.get('PG_DB_NAME', 'NONE'))
-        pg_username = str(environment_values.get('PG_USERNAME', 'NONE'))
-        pg_password = str(environment_values.get('PG_PASSWORD', 'NONE'))
+        if (env_file_path.exists() and env_file_path.is_file()):
+            environment_values = dotenv_values(env_file_path)
+            snow_api_url_for_stats = str(environment_values.get('SNOW_URL_STATS', 'NONE'))
+            snow_api_url_for_data = str(environment_values.get('SNOW_URL_DATA', 'NONE'))
+            snow_username = str(environment_values.get('SNOW_USERNAME', 'NONE'))
+            snow_password = str(environment_values.get('SNOW_PASSWORD', 'NONE'))
+            pg_host = str(environment_values.get('PG_HOST', 'LOCALHOST'))
+            pg_port = str(environment_values.get('PG_PORT', '5432'))
+            pg_database = str(environment_values.get('PG_DB_NAME', 'NONE'))
+            pg_username = str(environment_values.get('PG_USERNAME', 'NONE'))
+            pg_password = str(environment_values.get('PG_PASSWORD', 'NONE'))
+        else:
+            print(f'ERROR - ".env" File Is Not Present')
+            sys.exit(1)
     except Exception as error:
         print(f'ERROR - [S04] - {str(error)}')
 
@@ -53,3 +56,19 @@ if __name__ == '__main__':
         }
     except Exception as error:
         print(f'ERROR - [S05] - {str(error)}')
+
+    # fetching total incident ticket count:S06
+    try:
+        ticket_count_backend_response = total_ticket_count(snow_url = str(snow_api_url_for_stats), username = str(snow_username), password = str(snow_password), ticket_type = 'incident')
+        # check the result
+        if ((str(ticket_count_backend_response['status']).lower()) == 'success'):
+            incident_ticket_count = ticket_count_backend_response['ticket_count']
+            print(incident_ticket_count)
+        elif ((str(ticket_count_backend_response['status']).lower()) == 'error'):
+            print(f"ERROR - {ticket_count_backend_response['message']}")
+            sys.exit(1)
+        else:
+            print('ERROR - Total Ticket Count Not Fetched')
+            sys.exit(1)
+    except Exception as error:
+        print(f'ERROR - [S06] - {str(error)}')
